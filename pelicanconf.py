a0c96754eace95beea6cf03cb53fab7782540831
timger <yishenggudou@gmail.com>
2014-06-06 09:09:05 +0800

    脚本自动提交不知道写什么

---
+++ pelicanconf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'@timger'
+SITENAME = u'@timger'
+SITEURL = 'http://www.timger.info'
+
+
+TWITTER_USERNAME = 'yishenggudou'
+
+THEME='notmyidea'
+THEME='/Library/Python/2.7/site-packages/pelican/themes/notmyidea'
+CSS_FILE = "wide.css"
+
+DEFAULT_CATEGORY ='Others'
+OUTPUT_PATH = './output'
+PATH = './content'
